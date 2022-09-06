6413c92c9471cdfe0231c1f7152b02c1cfaa2526
Oleksandra <sashla1713@gmail.com>
2022-09-07 00:09:37 +0300

    update burger menu and introduse adaptive

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@ const headerBurger = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__menu');
 const back = document.querySelector('body');
 const headerList = document.querySelector('.menu__body');
-const menuContacts = document.querySelector('.contacts__info-menu');
-
 
 const modalBtn = document.querySelector('.modal-button');
 const modalWindow = document.querySelector('.modal');
@@ -17,13 +15,15 @@ headerBurger.onclick = function(){
     headerBurger.classList.toggle('active');
     headerMenu.classList.toggle('active');
     back.classList.toggle('lock');
-    menuContacts.classList.toggle('hide');
-    menuContacts.classList.toggle('show');
 
+    console.log(headerMenu.classList);
+    div.innerHTML='<div class="contacts__info-menu"><div class="contact__info-menu"><img src="img/icons/location.png" class="logo-location" alt="location"><span class="contact-location info-text-menu">92 Street St, New Yoark, NY 10013</span></div><div class="contact__info-menu"><img src="img/icons/tel.png" class="logo-telephone" alt="tel"><span class="contact-number info-text-menu">+1 800 235 958 556</span></div><div class="header__button"><button class="modal-button-menu button">Call back</button></div></div>';
+    headerMenu.append(div);
 };
 
 headerList.onclick = function () {
     headerList.classList.remove('active');
+    div.innerHTML='';
     back.classList.toggle('lock');
 };
 
@@ -54,12 +54,6 @@ document.addEventListener('keydown',(e) =>{
     closeModalWindow();
 });
 
-/* introduceContainer.addEventListener('resize',()=>{
-    const introduceWidth = introduceContainer.clientWidth;
-    if (introduceWidth<=850){
-        console.log('<850');
-    }
-}); */
 if(headerMenu.classList.contains('active')){
     
 }
